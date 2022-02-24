a3a7d36e400ce9dc6df441c4498608ec631381cf
a-sh <iam.angie@ymail.com>
2022-02-24 17:44:19 +1100

    Setup all DOM elements

--- app.js
+++ app.js
@@ -1,14 +1,60 @@
 // DOM Elements
 const hourEl = document.querySelector('.hour');
 const minuteEl = document.querySelector('.minute');
+const displayEl = document.querySelector('.display');
+
+// Function elements
+const acEl = document.querySelector('.ac');
+const pmEl = document.querySelector('.pm');
+const percentEl = document.querySelector('.percent');
+
+// Operator elements
+const additionEl = document.querySelector('.addition');
+const subtractionEl = document.querySelector('.subtraction');
+const divisionEl = document.querySelector('.division');
+const multiplicationEl = document.querySelector('.multiplication');
+const equalEl = document.querySelector('.equal');
+
+// Numbers elements
+const decimalEl = document.querySelector('.decimal');
+const number0El = document.querySelector('.number-0');
+const number1El = document.querySelector('.number-1');
+const number2El = document.querySelector('.number-2');
+const number3El = document.querySelector('.number-3');
+const number4El = document.querySelector('.number-4');
+const number5El = document.querySelector('.number-5');
+const number6El = document.querySelector('.number-6');
+const number7El = document.querySelector('.number-7');
+const number8El = document.querySelector('.number-8');
+const number9El = document.querySelector('.number-9');
+const numberElArray = [
+  number0El,
+  number1El,
+  number2El,
+  number3El,
+  number4El,
+  number5El,
+  number6El,
+  number7El,
+  number8El,
+  number9El,
+];
 
 // Setup time
-setInterval(() => {
+const updateTime = () => {
   const currentTime = new Date();
 
-  const currentHour = currentTime.getHours();
+  let currentHour = currentTime.getHours();
   const currentMinute = currentTime.getMinutes();
 
-  hourEl.textContent = currentHour.toString();
-  minuteEl.textContent = currentMinute.toString();
-}, 1000);
+  // ! set to 12hrs instead of 24hrs
+  // if (currentHour > 12) {
+  //   currentHour -= 12;
+  // }
+
+  hourEl.textContent = currentHour.toString().padStart(2, '0');
+  minuteEl.textContent = currentMinute.toString().padStart(2, '0');
+};
+
+setInterval(updateTime, 1000);
+updateTime();
