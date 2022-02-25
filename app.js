b31b57ece45e5f4ad0013c75fc96332db6659aa5
a-sh <iam.angie@ymail.com>
2022-02-25 19:02:23 +1100

    Limit max digit and configure dynamic fontSize + refactor code

--- app.js
+++ app.js
@@ -21,6 +21,18 @@ let valueStrInMemory = null;
 let operatorInMemory = null;
 
 // Functions
+const resetMemory = () => {
+  if (valueStrInMemory) {
+    valueStrInMemory = null;
+  }
+  if (operatorInMemory) {
+    operatorInMemory = null;
+  }
+};
+
+const getNumOfDigits = (valueStr) =>
+  [...valueStr].filter((val) => /\d/.test(val)).length;
+
 const getValueAsStr = () => {
   const currentDisplayStr = valueEl.textContent;
   return currentDisplayStr.split(',').join('');
@@ -31,6 +43,19 @@ const getValueAsNum = () => {
 };
 
 const setStrAsValue = (valueStr) => {
+  // create dynamic font-size for the number of values/digits produced
+  const digitCount = getNumOfDigits(valueStr);
+
+  if (digitCount <= 6) {
+    valueEl.style.fontSize = `clamp(4rem, 1rem + 4vw, 6rem)`;
+  } else if (digitCount === 7) {
+    valueEl.style.fontSize = `clamp(3.65rem, 1rem + 3.65vw, 5.65rem)`;
+  } else if (digitCount === 8) {
+    valueEl.style.fontSize = `clamp(3.25rem, 1rem + 3.25vw, 5.25rem)`;
+  } else if (digitCount >= 9) {
+    valueEl.style.fontSize = `clamp(2.85rem, 1rem + 2.85vw, 4.85rem)`;
+  }
+
   if (valueStr.at(-1) === '.') {
     valueEl.textContent += '.';
     return;
@@ -41,16 +66,6 @@ const setStrAsValue = (valueStr) => {
       parseFloat(wholeNumStr).toLocaleString() + '.' + decimalStr;
   } else {
     valueEl.textContent = parseFloat(valueStr).toLocaleString();
-  }
-};
-
-const handleNumberClick = (numStr) => {
-  const currentValueStr = getValueAsStr();
-
-  if (currentValueStr === '0') {
-    setStrAsValue(numStr);
-  } else {
-    setStrAsValue(currentValueStr + numStr);
   }
 };
 
@@ -76,6 +91,21 @@ const getResultOfOperationAsStr = () => {
   return newValueNum.toString();
 };
 
+// Event handler functions
+const handleNumberClick = (numStr) => {
+  const currentValueStr = getValueAsStr();
+  const currentValueCount = getNumOfDigits(currentValueStr);
+
+  // limit digit to max 9
+  if (currentValueCount >= 9) return;
+
+  if (currentValueStr === '0') {
+    setStrAsValue(numStr);
+  } else {
+    setStrAsValue(currentValueStr + numStr);
+  }
+};
+
 const handleOperatorClick = (operation) => {
   const currentValueStr = getValueAsStr();
 
@@ -91,15 +121,12 @@ const handleOperatorClick = (operation) => {
   setStrAsValue('0');
 };
 
-// Event Listeners to Functions
-acEl.addEventListener('click', () => {
+const handleAcClick = () => {
   setStrAsValue('0');
-  // clear memory
-  valueStrInMemory = null;
-  operatorInMemory = null;
-});
+  resetMemory();
+};
 
-pmEl.addEventListener('click', () => {
+const handlePmClick = () => {
   const currentValueNum = getValueAsNum();
   const currentValueStr = getValueAsStr();
 
@@ -113,16 +140,19 @@ pmEl.addEventListener('click', () => {
   } else {
     setStrAsValue(currentValueStr.substring(1));
   }
-});
+};
 
-percentEl.addEventListener('click', () => {
+const handlePercentClick = () => {
   const currentValueNum = getValueAsNum();
   const newValueNum = currentValueNum / 100;
   setStrAsValue(newValueNum.toString());
-  // clear memory
-  valueStrInMemory = null;
-  operatorInMemory = null;
-});
+  resetMemory();
+};
+
+// Event Listeners to Functions
+acEl.addEventListener('click', handleAcClick);
+pmEl.addEventListener('click', handlePmClick);
+percentEl.addEventListener('click', handlePercentClick);
 
 // Event Listeners to Operatros
 additionEl.addEventListener('click', () => {
@@ -145,8 +175,7 @@ equalEl.addEventListener('click', () => {
   if (valueStrInMemory) {
     setStrAsValue(getResultOfOperationAsStr());
     // reset memory
-    valueStrInMemory = null;
-    operatorInMemory = null;
+    resetMemory();
   }
 });
 
