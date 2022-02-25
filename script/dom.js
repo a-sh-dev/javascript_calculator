ba204c1362d5a9446eb29fbe5c33b761adb81530
a-sh <iam.angie@ymail.com>
2022-02-25 18:08:13 +1100

    Seperate dom els to a different file to cleanup

---
+++ script/dom.js
@@ -0,0 +1,41 @@
+// DOM Elements
+export const hourEl = document.querySelector('.hour');
+export const minuteEl = document.querySelector('.minute');
+export const valueEl = document.querySelector('.value');
+
+// Function elements
+export const acEl = document.querySelector('.ac');
+export const pmEl = document.querySelector('.pm');
+export const percentEl = document.querySelector('.percent');
+
+// Operator elements
+export const additionEl = document.querySelector('.addition');
+export const subtractionEl = document.querySelector('.subtraction');
+export const divisionEl = document.querySelector('.division');
+export const multiplicationEl = document.querySelector('.multiplication');
+export const equalEl = document.querySelector('.equal');
+
+// Numbers elements
+export const decimalEl = document.querySelector('.decimal');
+export const number0El = document.querySelector('.number-0');
+export const number1El = document.querySelector('.number-1');
+export const number2El = document.querySelector('.number-2');
+export const number3El = document.querySelector('.number-3');
+export const number4El = document.querySelector('.number-4');
+export const number5El = document.querySelector('.number-5');
+export const number6El = document.querySelector('.number-6');
+export const number7El = document.querySelector('.number-7');
+export const number8El = document.querySelector('.number-8');
+export const number9El = document.querySelector('.number-9');
+export const numberElArray = [
+  number0El,
+  number1El,
+  number2El,
+  number3El,
+  number4El,
+  number5El,
+  number6El,
+  number7El,
+  number8El,
+  number9El,
+];
